1abdf261a75d875764a038ec322325a727980253
AHT <alehertoro725@gmail.com>
2021-05-13 08:30:36 -0500

    added some code to movingBar class

--- seguimientoExceptions/src/model/movingBar.java
+++ seguimientoExceptions/src/model/movingBar.java
@@ -2,10 +2,20 @@ package model;
 
 public class movingBar 
 {
+	int barX, barY, barSizeX, barSizeY;
+	int knobX, knobY, knobSizeX, knobSizeY;
 
-	public movingBar() 
+	public movingBar(int barX, int barY, int knobX, int knobY) 
 	{
+		this.barSizeX = 400;
+		this.barSizeY = 15;
+		this.knobSizeX = 15;
+		this.knobSizeY = 40;
 		
+		this.barX = barX;
+		this.barY = barY;
+		this.knobX = knobX;
+		this.knobY = knobY;
 	}
 
 }
